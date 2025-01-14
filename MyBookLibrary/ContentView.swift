b62de438231b58efc40f5195eb5d841dc191f8f0
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 17:57:39 +0300

    feat: View finalized for the last updates.

--- MyBookLibrary/ContentView.swift
+++ MyBookLibrary/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            BookListView()
+                .tabItem {
+                    Image(systemName: "books.vertical.fill")
+                    Text("Books")
+                }
+            
+            GenreListView()
+                .tabItem {
+                    Image(systemName: "gear.circle")
+                    Text("Genre")
+                }
         }
-        .padding()
     }
 }
 
