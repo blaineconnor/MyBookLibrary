21814c3ef37127b79c3e6a33dabeab8a512eda9d
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 17:53:58 +0300

    feat: Genre listing operated.

---
+++ MyBookLibrary/Genres/Views/GenreListView.swift
@@ -0,0 +1,46 @@
+//
+//  GenreListView.swift
+//  MyBookLibrary
+//
+//  Created by Fatih Emre on 14.01.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct GenreListView: View {
+    
+    @State private var presentAddNew = false
+    @State private var sortOrder: GenreSortOrder = .forward
+    
+    var body: some View {
+        NavigationStack {
+            GenreListSubview(sortOrder: sortOrder)
+            .navigationTitle("Genres")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        presentAddNew.toggle()
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .sheet(isPresented: $presentAddNew) {
+                        AddNewGenre()
+                            .presentationDetents([.fraction(0.4)])
+                            .interactiveDismissDisabled()
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sortOrder = sortOrder == GenreSortOrder.forward ? GenreSortOrder.reverse : GenreSortOrder.forward
+                    } label: {
+                        Image(systemName: "arrow.up.arrow.down")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+        }
+    }
+}
