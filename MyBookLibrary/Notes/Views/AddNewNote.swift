003cc1ff329520d68c582756f2441352f02e5255
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 17:55:50 +0300

    feat:  adding note to books is implemented and organised.

---
+++ MyBookLibrary/Notes/Views/AddNewNote.swift
@@ -0,0 +1,51 @@
+//
+//  AddNewNote.swift
+//  MyBookLibrary
+//
+//  Created by Fatih Emre on 14.01.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddNewNote: View {
+    let book: Book
+    
+    @State private var title: String = ""
+    @State private var message: String = ""
+    
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+        
+    var body: some View {
+        Form {
+            TextField("Note title", text: $title)
+            TextField("Note", text: $message)
+        }
+        .navigationTitle("Add new note")
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    let note = Note(title: title, message: message)
+                    note.book = book
+                    context.insert(book)
+                    
+                    do {
+                        try context.save()
+                        book.notes.append(note)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                    
+                    dismiss()
+                }
+            }
+        }
+    }
+}
