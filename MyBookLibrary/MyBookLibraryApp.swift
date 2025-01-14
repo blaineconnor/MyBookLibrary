4b89d2065db16b86a44f05f9d533f563fe7dd0a9
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 12:36:46 +0300

    Initial Commit

---
+++ MyBookLibrary/MyBookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookLibraryApp.swift
+//  MyBookLibrary
+//
+//  Created by Fatih Emre on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyBookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
