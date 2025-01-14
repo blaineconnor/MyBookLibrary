b02c8fbbfa7bced69849f757f3efe9deffb84408
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-14 17:52:24 +0300

    feat: Book listing operated.

---
+++ MyBookLibrary/Books/Views/BookCellView.swift
@@ -0,0 +1,38 @@
+//
+//  BookCellView.swift
+//  MyBookLibrary
+//
+//  Created by Fatih Emre on 14.01.2025.
+//
+
+import SwiftUI
+
+struct BookCellView: View {
+    let book: Book
+    
+    var body: some View {
+        NavigationLink(value: book) {
+            HStack(alignment: .top) {
+                if let cover = book.cover, let image = UIImage(data: cover) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(.rect(cornerRadius: 5))
+                        .frame(height: 100)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text(book.title)
+                        .bold()
+                    
+                    Group {
+                        Text("Author \(book.author)")
+                        Text("Published on: \(book.publishedYear.description)")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                }
+            }
+        }
+    }
+}
---
+++ MyBookLibrary/Books/Views/BookListView.swift
@@ -0,0 +1,71 @@
+//
+//  BookListView.swift
+//  MyBookLibrary
+//
+//  Created by Fatih Emre on 14.01.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct BookListView: View {
+    @Environment(\.modelContext) private var context
+    @Query private var books: [Book]
+    @State private var presentAddNew = false
+    
+    @State private var searchTerm: String = ""
+    
+    var filteredBooks: [Book] {
+        guard searchTerm.isEmpty == false else {return books}
+        return books.filter {
+            $0.title.localizedCaseInsensitiveContains(searchTerm)
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(filteredBooks) { book in
+                    BookCellView(book: book)
+                }
+                .onDelete(perform: delete(indexSet:))
+            }
+            .searchable(text: $searchTerm, prompt: "Search book title")
+            .navigationTitle("My Books")
+            .navigationDestination(for: Book.self) { book in
+                BookDetailView(book: book)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    
+                    Button {
+                        presentAddNew.toggle()
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .sheet(isPresented: $presentAddNew, content: {
+                        AddNewBookView()
+                    })
+                }
+            }
+        }
+    }
+    private func delete(indexSet: IndexSet) {
+        indexSet.forEach { index in
+            let book = books[index]
+            context.delete(book)
+            
+            do{
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    BookListView()
+}
